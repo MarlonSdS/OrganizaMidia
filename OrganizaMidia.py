0624cfbfad1fde51b6734c5a3c09915bea118243
Marlon dos Santos <marlondossantos03@outlook.com>
2024-07-09 20:41:42 -0300

    listagem de arquivos e separação por tipo

--- OrganizaMidia.py
+++ OrganizaMidia.py
@@ -1,6 +1,6 @@
 from shutil import make_archive
 from datetime import datetime
-from os import getlogin
+from os import getlogin, listdir
 
 #data de hoje
 today = datetime.today().strftime('%d-%m-%Y')
@@ -8,6 +8,7 @@ today = datetime.today().strftime('%d-%m-%Y')
 #usuário do sistema
 user = getlogin()
 
+#LISTAS
 #nomes dos arquivos de cada backup
 filenames = ["ImagensBackup-"+today,
              "DocumentosBackup-"+today,
@@ -17,6 +18,41 @@ filenames = ["ImagensBackup-"+today,
 sources = [f'/home/{user}/Imagens',
            f'/home/{user}/Documentos',
            f'/home/{user}/Vídeos']
+
+#lista de tipos de arquivos de imagens
+img_types = ['.jpeg', '.jpg', '.png', '.svg', '.gif']
+doc_types = ['.txt', '.odt', '.docx', 'pdf', '.epub', '.html', '.rtf']
+vid_types = ['.mp4', '.mkv', '.mov', '.avi']
+
+#lista os arquivos de downloads
+def list_files():
+    files = listdir(f'/home/{user}/Downloads/')
+    return files
+
+#SEPARAÇÃO DE ARQUIVOS POR TIPO
+def separate_images(files):
+    images = []
+    for arq in files:
+        for mime in img_types:
+            if mime in arq:
+                images.append(arq)
+    return images
+
+def separate_documents(files):
+    docs = []
+    for arq in files:
+        for mime in doc_types:
+            if mime in arq:
+                docs.append(arq)
+    return docs
+
+def separate_videos(files):
+    videos = []
+    for arq in files:
+        for mime in vid_types:
+            if mime in arq:
+                videos.append(arq)
+    return videos
 
 #checa se já é hora de fazer o backup
 def check_time():
